03e0914ced5f66b3eecd82c783f62dbc76484dc2
Zaco <cyrilmathe08@gmail.com>
2025-11-13 10:41:49 +0100

    :art: amélioration exercice liste de pokemons

--- vite/pokemon-list/src/App.jsx
+++ vite/pokemon-list/src/App.jsx
@@ -1,9 +1,8 @@
 import {
   QueryClient,
   QueryClientProvider,
-  useQuery,
-  useQueryClient,
 } from '@tanstack/react-query'
+import PokemonList from './components/PokemonList'
 
 const queryClient = new QueryClient()
 
@@ -12,32 +11,5 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <PokemonList />
     </QueryClientProvider>
-  )
-}
-
-function PokemonList() {
-  const queryClient = useQueryClient()
-  const { isPending, error, data } = useQuery({
-    queryKey: ['pokemon'],
-    queryFn: () =>
-      fetch('https://pokeapi.co/api/v2/pokemon?limit=20').then((res) =>
-        res.json(),
-      ),
-  })
-
-  if (isPending) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.message
-
-  return (
-    <div>
-      <p>Liste de 20 pokémons :</p>
-      <ul>
-        {data.results.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
-      </ul>
-      <button onClick={() => {console.log("refresh"), queryClient.invalidateQueries({ queryKey: ['pokemon'] })}}>refresh</button>
-    </div>
   )
 }
---
+++ vite/pokemon-list/src/components/PokemonList.jsx
@@ -0,0 +1,32 @@
+
+import {
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query'
+
+export default function PokemonList() {
+  const queryClient = useQueryClient()
+  const { isPending, error, data } = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: () =>
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=20').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      <p>Liste de 20 pokémons :</p>
+      <ul>
+        {data.results.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+      <button onClick={() => {console.log("refresh"), queryClient.invalidateQueries({ queryKey: ['pokemon'] })}}>refresh</button>
+    </div>
+  )
+}
